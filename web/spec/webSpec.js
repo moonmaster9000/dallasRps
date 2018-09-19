3a1e37b9b000bbe49a82bda99fb16852483ec8db
Matt Parker <mparker@pivotal.io>
2018-09-19 15:05:51 -0500

    failing frontend test

--- web/spec/webSpec.js
+++ web/spec/webSpec.js
@@ -1,5 +1,27 @@
-describe("foo", function () {
-    it("bars", function () {
-        expect(true).toBe(false)
+const React = require("react")
+const ReactDOM = require("react-dom")
+
+class PlayForm extends React.Component {
+    render(){
+        return <button>Hello World!</button>
+    }
+}
+
+describe("play form", function () {
+    describe("when the game module determines the throws are invalid", function () {
+        it("displays 'INVALID'", function () {
+            let domFixture = document.createElement("div")
+            domFixture.id = "hello"
+            document.body.appendChild(domFixture)
+
+            let requests = {
+                play(p1Throw, p2Throw, ui){ ui.invalid() }
+            }
+
+            ReactDOM.render(<PlayForm requests={requests}/>, domFixture)
+
+            domFixture.querySelector("button").click()
+            expect(domFixture.innerText).toContain("INVALID")
+        })
     })
 })
